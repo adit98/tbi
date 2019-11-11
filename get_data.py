acc355af8e68656dcedff0a6135867032794ee6d
Adit Murali <adit.murali@outlook.com>
2019-11-11 10:33:56 -0500

    asdf

--- get_data.py
+++ get_data.py
@@ -13,70 +13,80 @@ conn = psql.connect(conn_string)
 print("Connected!")
 cursor = conn.cursor()
 
-queries = {'physiology_query' : "SELECT * \
-    FROM eicu_crd.apacheApsVar INNER JOIN eicu_crd.diagnosis \
-    ON eicu_crd.apacheApsVar.patientunitstayid = eicu_crd.diagnosis.patientunitstayid \
-    WHERE (SUBSTRING(diagnosisstring, 1,25) = 'burns/trauma|trauma - CNS' \
-    OR SUBSTRING(diagnosisstring, 1,23) = 'neurologic|trauma - CNS') \
-    AND diagnosisstring NOT LIKE '%spinal cord%' LIMIT {} OFFSET {};",
+#'infusion_query' : "with p_id as ( \
+#        select distinct patientunitstayid, diagnosisstring \
+#        from eicu_crd.diagnosis \
+#        WHERE diagnosisstring LIKE '%trauma - CNS|intracranial%' \
+#    ) \
+#    SELECT * \
+#    FROM p_id INNER JOIN eicu_crd.infusionDrug \
+#    ON eicu_crd.infusionDrug.patientunitstayid = p_id.patientunitstayid \
+#    LIMIT {} OFFSET {};",
 
-'apache_pred_query' : "SELECT * \
-    FROM eicu_crd.apachePredVar INNER JOIN eicu_crd.diagnosis \
-    ON eicu_crd.apachePredVar.patientunitstayid = eicu_crd.diagnosis.patientunitstayid \
-    WHERE (SUBSTRING(diagnosisstring, 1,25) = 'burns/trauma|trauma - CNS' \
-    OR SUBSTRING(diagnosisstring, 1,23) = 'neurologic|trauma - CNS') \
-    AND diagnosisstring NOT LIKE '%spinal cord%' LIMIT {} OFFSET {};",
 
-'infusion_query' : "SELECT * \
-    FROM eicu_crd.infusionDrug INNER JOIN eicu_crd.diagnosis \
-    ON eicu_crd.infusionDrug.patientunitstayid = eicu_crd.diagnosis.patientunitstayid \
-    WHERE (SUBSTRING(diagnosisstring, 1,25) = 'burns/trauma|trauma - CNS' \
-    OR SUBSTRING(diagnosisstring, 1,23) = 'neurologic|trauma - CNS') \
-    AND diagnosisstring NOT LIKE '%spinal cord%' LIMIT {} OFFSET {};",
+queries = {
+    'nurse_charting_query' : "with p_id as ( \
+            select distinct patientunitstayid, diagnosisstring \
+            from eicu_crd.diagnosis \
+            WHERE diagnosisstring LIKE '%trauma - CNS|intracranial%' \
+        ) \
+        SELECT p_id.patientunitstayid, nursingchartoffset, \
+        nursingchartentryoffset, nursingchartcelltypevallabel, \
+        nursingchartcelltypevalname, nursingchartvalue \
+        FROM p_id INNER JOIN eicu_crd.nursecharting \
+        ON p_id.patientunitstayid = eicu_crd.nursecharting.patientunitstayid \
+        WHERE nursingchartcelltypevallabel = 'Glasgow coma score' \
+        LIMIT {} OFFSET {};",
 
-'nurse_charting_query' : "SELECT * \
-    FROM eicu_crd.diagnosis \
-    INNER JOIN eicu_crd.nursecharting \
-    ON eicu_crd.diagnosis.patientunitstayid = eicu_crd.nursecharting.patientunitstayid \
-    WHERE nursingchartcelltypevallabel = 'Glasgow coma score' \
-    AND (SUBSTRING(diagnosisstring, 1,25) = 'burns/trauma|trauma - CNS' \
-    OR SUBSTRING(diagnosisstring, 1,23) = 'neurologic|trauma - CNS') \
-    AND diagnosisstring not like '%spinal cord%' LIMIT {} OFFSET {};",
+    'medication_query' : "with p_id as ( \
+            select distinct patientunitstayid, diagnosisstring \
+            from eicu_crd.diagnosis \
+            WHERE diagnosisstring LIKE '%trauma - CNS|intracranial%' \
+        ) \
+        SELECT * \
+        FROM p_id INNER JOIN eicu_crd.medication \
+        ON eicu_crd.medication.patientunitstayid = p_id.patientunitstayid \
+        LIMIT {} OFFSET {};",
 
-'medication_query' : "SELECT * \
-    FROM eicu_crd.medication INNER JOIN eicu_crd.diagnosis \
-    ON eicu_crd.medication.patientunitstayid = eicu_crd.diagnosis.patientunitstayid \
-    WHERE (SUBSTRING(diagnosisstring, 1,25) = 'burns/trauma|trauma - CNS' \
-    OR SUBSTRING(diagnosisstring, 1,23) = 'neurologic|trauma - CNS') \
-    AND diagnosisstring NOT LIKE '%spinal cord%' LIMIT {} OFFSET {};",
+    'patient_demographics_query' : "with p_id as ( \
+            select distinct patientunitstayid, diagnosisstring \
+            from eicu_crd.diagnosis \
+            WHERE diagnosisstring LIKE '%trauma - CNS|intracranial%' \
+        ) \
+        SELECT * \
+        FROM p_id INNER JOIN eicu_crd.patient \
+        ON eicu_crd.patient.patientunitstayid = p_id.patientunitstayid \
+        LIMIT {} OFFSET {};",
 
-'patient_demographics_query' : "SELECT * \
-    FROM eicu_crd.patient INNER JOIN eicu_crd.diagnosis \
-    ON eicu_crd.patient.patientunitstayid = eicu_crd.diagnosis.patientunitstayid \
-    WHERE (SUBSTRING(diagnosisstring, 1,25) = 'burns/trauma|trauma - CNS' \
-    OR SUBSTRING(diagnosisstring, 1,23) = 'neurologic|trauma - CNS') \
-    AND diagnosisstring NOT LIKE '%spinal cord%' LIMIT {} OFFSET {};",
+    'respiratoryCharting_query' : "with p_id as ( \
+            select distinct patientunitstayid, diagnosisstring \
+            from eicu_crd.diagnosis \
+            WHERE diagnosisstring LIKE '%trauma - CNS|intracranial%' \
+        ) \
+        SELECT * \
+        FROM p_id INNER JOIN eicu_crd.respiratoryCharting \
+        ON eicu_crd.respiratoryCharting.patientunitstayid = p_id.patientunitstayid \
+        LIMIT {} OFFSET {};", \
 
-'respiratory_query' : "SELECT * \
-    FROM eicu_crd.respiratoryCharting INNER JOIN eicu_crd.diagnosis \
-    ON eicu_crd.respiratoryCharting.patientunitstayid = eicu_crd.diagnosis.patientunitstayid \
-    WHERE (SUBSTRING(diagnosisstring, 1,25) = 'burns/trauma|trauma - CNS' \
-    OR SUBSTRING(diagnosisstring, 1,23) = 'neurologic|trauma - CNS') \
-    AND diagnosisstring NOT LIKE '%spinal cord%' LIMIT {} OFFSET {};",
+    'aperiodic_vitals_query' : "with p_id as ( \
+            select distinct patientunitstayid, diagnosisstring \
+            from eicu_crd.diagnosis \
+            WHERE diagnosisstring LIKE '%trauma - CNS|intracranial%' \
+        ) \
+        SELECT * \
+        FROM p_id INNER JOIN eicu_crd.vitalAperiodic \
+        ON eicu_crd.vitalAperiodic.patientunitstayid = p_id.patientunitstayid \
+        LIMIT {} OFFSET {};",
 
-'aperiodic_vitals_query' : "SELECT * \
-    FROM eicu_crd.vitalAperiodic INNER JOIN eicu_crd.diagnosis \
-    ON eicu_crd.vitalAperiodic.patientunitstayid = eicu_crd.diagnosis.patientunitstayid \
-    WHERE (SUBSTRING(diagnosisstring, 1,25) = 'burns/trauma|trauma - CNS' \
-    OR SUBSTRING(diagnosisstring, 1,23) = 'neurologic|trauma - CNS') \
-    AND diagnosisstring NOT LIKE '%spinal cord%' LIMIT {} OFFSET {};",
-
-'periodic_vitals_query' : "SELECT * \
-    FROM eicu_crd.vitalPeriodic INNER JOIN eicu_crd.diagnosis \
-    ON eicu_crd.vitalPeriodic.patientunitstayid = eicu_crd.diagnosis.patientunitstayid \
-    WHERE (SUBSTRING(diagnosisstring, 1,25) = 'burns/trauma|trauma - CNS' \
-    OR SUBSTRING(diagnosisstring, 1,23) = 'neurologic|trauma - CNS') \
-    AND diagnosisstring NOT LIKE '%spinal cord%' LIMIT {} OFFSET {};"}
+    'periodic_vitals_query' : "with p_id as ( \
+            select distinct patientunitstayid, diagnosisstring \
+            from eicu_crd.diagnosis \
+            WHERE diagnosisstring LIKE '%trauma - CNS|intracranial%' \
+        ) \
+        SELECT * \
+        FROM p_id INNER JOIN eicu_crd.vitalPeriodic \
+        ON eicu_crd.vitalPeriodic.patientunitstayid = p_id.patientunitstayid \
+        LIMIT {} OFFSET {};"}
 
 for key in queries.keys():
     offset = 0
